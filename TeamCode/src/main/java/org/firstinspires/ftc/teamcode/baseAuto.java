81a4b877ec1be2d41236379b62f904aba6c5a689
ekrolick <ekrolick26@klschools.org>
2025-10-28 07:58:35 -0400

    this might work better

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/baseAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/baseAuto.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode; // make sure this aligns with class location
+package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.pedropathing.follower.Follower;
@@ -8,38 +8,31 @@ import com.pedropathing.pathgen.PathChain;
 import com.pedropathing.pathgen.Point;
 import com.pedropathing.util.Constants;
 import com.pedropathing.util.Timer;
-import com.qualcomm.hardware.limelightvision.LLResult;
-import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
-
 
 import pedroPathing.constants.FConstants;
 import pedroPathing.constants.LConstants;
 
-
+@Config
 @Autonomous(name = "Example Auto", group = "Examples")
 public class baseAuto extends OpMode {
 
     private Follower follower;
     private Timer pathTimer, actionTimer, opmodeTimer;
 
-    private final Pose start1 = new Pose(0, 0, Math.toRadians(0));
-    private final Pose pos1 = new Pose(pos1x, pos1y, Math.toRadians(0));
     public static double start1x = 0.0;
     public static double start1y = 0.0;
     public static double pos1x = 2.0;
     public static double pos1y = 2.0;
-    String pathState = "init";
+
+    private final Pose start1 = new Pose(start1x, start1y, Math.toRadians(0));
+    private final Pose pos1 = new Pose(pos1x, pos1y, Math.toRadians(0));
 
     private PathChain fMove;
+    private String pathState = "init";
 
     public void buildPaths() {
-        // Path for scoring preload
         fMove = follower.pathBuilder()
                 .addPath(new BezierLine(new Point(start1), new Point(pos1)))
                 .setLinearHeadingInterpolation(start1.getHeading(), pos1.getHeading())
@@ -49,7 +42,15 @@ public class baseAuto extends OpMode {
     @Override
     public void init() {
         follower = new Follower(hardwareMap);
+        pathTimer = new Timer();
+        actionTimer = new Timer();
+        opmodeTimer = new Timer();
         buildPaths();
+    }
+
+    @Override
+    public void start() {
+        setPathState("move off line");
     }
 
     @Override
@@ -57,26 +58,22 @@ public class baseAuto extends OpMode {
         follower.update();
         autonomousPathUpdate();
         telemetry.addData("path state", pathState);
+        telemetry.update();
     }
-
-    public void start() {
-
-    }
-
-
 
     public void autonomousPathUpdate() {
         switch (pathState) {
-        case "move off line":
-        if (!follower.isBusy()) {
-            follower.followPath(fMove, true);
-            setPathState("move to bar");
-        }
-        break;
+            case "move off line":
+                if (!follower.isBusy()) {
+                    follower.followPath(fMove, true);
+                    setPathState("done");
+                }
+                break;
 
+        }
     }
-    }
-    public void setPathState (String pState){
+
+    public void setPathState(String pState) {
         pathState = pState;
         pathTimer.resetTimer();
     }
